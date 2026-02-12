2d66bf646be4e6fcb69093bc646ff4bef351320d
monsoon <97245589@qq.com>
2026-02-12 15:08:14 -0700

    leveldb

--- common/func/ldb.lua
+++ common/func/ldb.lua
@@ -4,21 +4,21 @@ local mode = ...
 
 if mode == "child" then
     -- del keys hkeys hset hget hdel compact
-
     skynet.start(function()
-        local db
+        local leveldb = require "lgame.leveldb"
+        local pdb
+
         skynet.dispatch("lua", function(_, _, cmd, ...)
-            if not db then
-                local ldb = require "lgame.leveldb"
-                db = ldb.create("db/" .. skynet.getenv("server_mark"))
+            if not pdb then
+                pdb = leveldb.create("db/" .. skynet.getenv("server_mark"))
             end
-            skynet.retpack(db[cmd](db, ...))
+            skynet.retpack(leveldb[cmd](pdb, ...))
         end)
     end)
 else
     local addr = skynet.uniqueservice("common/func/ldb", "child")
 
-    return function(...)
-        return skynet.call(addr, "lua", ...)
+    return function(cmd, ...)
+        return skynet.call(addr, "lua", cmd, ...)
     end
 end
