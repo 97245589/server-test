a624a4255a5b76a5137a29fed9e616bf0cf5bec2
monsoon <97245589@qq.com>
2026-02-14 14:07:15 -0700

    login test fork

--- server/test/login.lua
+++ server/test/login.lua
@@ -1,8 +1,4 @@
 require "common.func.tool"
-local require = require
-local tostring = tostring
-local print = print
-local dump = dump
 local skynet = require "skynet"
 local client = require "server.test.func.client"
 
@@ -11,28 +7,26 @@ local get_res = client.get_res
 local send_req = client.send_req
 
 local conn = function(args)
+    local fd = login(args)
+    send_req(fd, "get_data", {})
+
+    -- skynet.fork(function()
+    --     while true do
+    --         skynet.sleep(100)
+    --         send_req(fd, "get_data", {})
+    --     end
+    -- end)
+
     skynet.fork(function()
-        local fd = login(args)
-        send_req(fd, "get_data", {})
-
-        -- skynet.fork(function()
-        --     while true do
-        --         skynet.sleep(100)
-        --         send_req(fd, "get_data", {})
-        --     end
-        -- end)
-
-        skynet.fork(function()
-            while true do
-                print(fd, get_res(fd))
-            end
-        end)
+        while true do
+            print(fd, get_res(fd))
+        end
     end)
 end
 
 skynet.start(function()
     for i = 1, 1 do
-        conn({
+        skynet.fork(conn, {
             acc = "acc" .. i,
             playerid = i,
             gamehost = "127.0.0.1:10012",
